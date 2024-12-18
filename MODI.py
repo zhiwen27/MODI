075023a66f0d825608d7f5b861ba51000166a9f5
zhiwen27 <157720245+zhiwen27@users.noreply.github.com>
2024-12-18 18:54:49 -0500

    Update MODI.py

--- MODI.py
+++ MODI.py
@@ -1,31 +1,3 @@
-'''
-Using MODI method to solve transportation problem:
-1. Find an initial solution with m + n -1 occupied cells and calculate u[i] and v[j] for rows and columns using Least Cost Method.
-To start with, assign u[i] to be zero. Then calculate of u_s and v_s using C_ij = u_i + v_j. 
-2. For unoccupied cells, calculate opportunity cost = C_ij - u_i - v_j. 
-3. Test if basic feasible solution is optimal: 1). Check whether row(m) + col(n) - 1 = total number of allocated cells. If the total
-number of allocated cells is fewer than m + n - 1, the case of degeneracy occurs. To convert unoccupied cells into occupied cells,
-start from the least value among all the unoccupied cells and check if that cell has no closed-loop formation. If so, select it as a
-new occupied cell and assign with value epsilon. 2). Examine the sign of each opportunity cost cell: If opportunity_cost > 0, then
-current solution is optimal; If opportunity_cost = 0, then an alternative solution exists; If there exists opportunity_cost < 0, then
-an improved solution can be obtained by pivoting from the cell with the largest negative value of opportunity cost.
-4. Construct a closed loop from the cell with largest negative opportunity cost. Start the loop with the selected cell and mark it
-with a plus sign. Then, search along the row to an occupied cell, mark the cell with a minus sign and continue down the column to an
-occupied cell and mark the corner with a plus sign. Continue the search until the loop gets back to the selected cell.
-5. Find the smallest value among the cells marked with minus sign. Add it to cells marked with plus sign and subtract it from cells
-marked with minus sign.
-6.Check again for the revised solution to see if it's optimal.
-
-Some problems:
-1. Although I assigned epsilon to be 1.e-9, there might still be some small calculation errors when finding the total cost. The ideal
-case is to leave all the epsilons and multiply the solution with the cell's cost and then add all of them.
-2. Need to rewrite the code to be a non-recursive one. Both in search_row & search_col and in check_solution & find_loop (better use
-loops instead).
-3. When considering degeneracy, there might be the case when the cost cells have the same value, so even if I delete the first value
-in the sort list, I'm not sure if I didn't repeatedly check the cells.
-4. The inputs of the code can be rewritten so that it can just read from a file.
-'''
-
 import numpy as np
 import scipy as sp
 
